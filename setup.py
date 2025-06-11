d807526e1af469e5c1a634f6305241da3d8fa2c9
Amit Vaisman <102420929+AmitVaisman@users.noreply.github.com>
2025-06-11 11:52:40 +0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='know-subnet',
+      packages=['know_subnet'],
+      version='0.1',
+      description='A package to learn subnetwork masks over pretrained language models for relational knowledge.',
+      install_requires=[])
