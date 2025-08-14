bab7c4fc1177897adc3d48a15ecf1dc005544945
Amit Vaisman <amit.vaisman@campus.technion.ac.il>
2025-08-14 10:14:57 +0300

    vizis
    Signed-off-by: Amit Vaisman <amit.vaisman@campus.technion.ac.il>

--- setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup
-
-setup(name='know-subnet',
-      packages=['know_subnet'],
-      version='0.1',
-      description='A package to learn subnetwork masks over pretrained language models for relational knowledge.',
-      install_requires=[])
